20dcd81ff387506b83e9e6525e686ca4bc308072
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-06-15 23:14:32 +0000

    [Finish] フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :relationships, foreign_key: :following_id
   has_many :followings, through: :relationships, source: :follower
-  has_many :reverse_of_relationships, class_name: 'relationship', foreign_key: :follower_id
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
   has_many :followers, through: :reverse_of_relationships, source: :following
   has_one_attached :profile_image
 
