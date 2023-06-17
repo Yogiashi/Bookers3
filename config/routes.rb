c7b732691acff958d866220209b1f67d56ffe975
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-06-17 11:30:09 +0000

    [Add] 検索機能のコントローラとアクションの追加、ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root "homes#top"
   get "home/about"=>"homes#about"
+  get 'searche' => 'searches#search'
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
@@ -15,5 +16,6 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
